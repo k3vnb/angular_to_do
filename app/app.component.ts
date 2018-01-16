79cff79efe06debe7d4175803a5c0e6c87fd578d
Kevin Boyle and Lena Ha <kevin@>
2018-01-16 15:05:48 -0800

    add ngModel, 2-way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-    <ul>
-      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
-    </ul>
-  </div>
+      <div class="container">
+        <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+        <h3>{{currentFocus}}</h3>
+        <ul>
+          <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+        </ul>
+        <hr>
+        <div *ngIf="selectedTask">
+          <h3>{{selectedTask.description}}</h3>
+          <p>Task Complete? {{selectedTask.done}}</p>
+          <h3>Edit Task</h3>
+          <label>Enter Task Description:</label>
+          <input [(ngModel)]="selectedTask.description">
+          <label>Enter Task Priority (1-3):</label>
+          <br>
+          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+          <button (click)="finishedEditing()">Done</button>
+       </div>
+     </div>
   `
 })
 //class declaration
@@ -21,14 +35,41 @@ export class AppComponent {
   year: number = this.currentTime.getFullYear();
   //'this' is necessary b/c In Angular, when a variable in a component's class declaration references another variable in the class, it must be prefaced with the this keyword.
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
-  ];
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)   ];
+  selectedTask = null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("This task is DONE.");
+    } else {
+      alert("This ain't done - GET TO WORK.")
+    }
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
 }
 //below is our Task model
 export class Task {
+
   public done: boolean = false;
-  constructor(public description: string) { }
+  constructor(public description: string, public priority: number) { }
 }
 //because we've exported Task class, Task is now a valid data type for variables.
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,10 @@
 import { NgModule }      from '@angular/core'; //imports general Module code from Angular framework's core library
 import { BrowserModule } from '@angular/platform-browser'; //imports code needed to run app in browser. Has built in directives for conditionals & loops in components.
 import { AppComponent }   from './app.component'; //refers to the root component (check app/app.component.ts)
-
+import { FormsModule }  from '@angular/forms'; //two-way binding w/ ngModel is almost always used in forms.
 @NgModule({
-  imports: [ BrowserModule ], //array of other modules & content this module requires.
+  imports: [ BrowserModule,
+              FormsModule ], //array of other modules & content this module requires.
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
